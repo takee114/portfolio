dd7f5b8c9f50469ee89a7fafa9230e8d81e47c07
Tekalign Getachew <tekaligngetachew114@gmail.com>
2024-09-26 13:23:45 -0700

    added vercel contact form

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -1,7 +1,7 @@
 (function () {
   "use strict";
 
-  let forms = document.querySelectorAll(".nodeJs-email-api");
+  let forms = document.querySelectorAll(".vercel-email");
 
   forms.forEach(function (e) {
     e.addEventListener("submit", function (event) {
